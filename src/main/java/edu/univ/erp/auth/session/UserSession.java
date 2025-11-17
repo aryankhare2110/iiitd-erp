7d73ab8d1494609ef78e036cc56ea1c7ff351bf7
Aryan Khare <aryan24124@iiitd.ac.in>
2025-11-17 17:56:49 +0530

    Commit: UserSession Completed

--- src/main/java/edu/univ/erp/auth/session/UserSession.java
+++ src/main/java/edu/univ/erp/auth/session/UserSession.java
@@ -1,4 +1,36 @@
 package edu.univ.erp.auth.session;
 
 public class UserSession {
+
+    private static int userID;
+    private static String userEmail;
+    private static String userRole;
+
+    public static void startSession (int id, String email, String role) {
+        userID = id;
+        userEmail = email;
+        userRole = role;
+    }
+
+    public static int getUserID() {
+        return userID;
+    }
+
+    public static String getUserEmail() {
+        return userEmail;
+    }
+
+    public static String getUserRole() {
+        return userRole;
+    }
+
+    public static void clear() {
+        userID = 0;
+        userRole = null;
+        userEmail = null;
+    }
+
+    public static boolean isLoggedIn() {
+        return userEmail != null;
+    }
 }
