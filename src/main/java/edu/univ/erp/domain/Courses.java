eb2546668d1bf5b20ab28a08f7b12a23a389f115
Aryan Khare <aryan24124@iiitd.ac.in>
2025-11-18 17:04:23 +0530

    Commit: Added Courses.java

--- src/main/java/edu/univ/erp/domain/Courses.java
+++ src/main/java/edu/univ/erp/domain/Courses.java
@@ -1,4 +1,39 @@
 package edu.univ.erp.domain;
 
 public class Courses {
+
+    private int courseID;
+    private String code;
+    private String title;
+    private int credits;
+    private String prerequisites;
+
+    public Courses(int courseID, String code, String title, int credits, String prerequisites) {
+        this.courseID = courseID;
+        this.code = code;
+        this.title = title;
+        this.credits = credits;
+        this.prerequisites = prerequisites;
+    }
+
+    public int getCourseID() {
+        return courseID;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getCredits() {
+        return credits;
+    }
+
+    public String getPrerequisites() {
+        return prerequisites;
+    }
+
 }
